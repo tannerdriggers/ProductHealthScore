3cd62b454aa11d5e646ccbc08929dda4eafd481c
tannerdriggers <tannerdriggers@ksu.edu>
2021-07-19 22:58:17 -0500

    Init

---
+++ screens/BarcodeScannerScreen.tsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+export default function BarcodeScannerScreen({navigation}) {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+  const [product, setProduct] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = async ({ type, data }) => {
+    setScanned(true);
+    fetch(`https://world.openfoodfacts.org/api/v0/product/${data}.json`)
+      .then((response) => response.json())
+      .then((product) => {
+        setProduct(product);
+      })
+      .catch((error) => {
+        console.error("Error getting the food facts response.", error)
+      })
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  if (product) {
+    navigation.navigate('TabTwo', { product });
+  }
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+});
